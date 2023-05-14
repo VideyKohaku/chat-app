d533e4ad2c0118e814fb9caa3587339c50ead494
unknown <nguyenphuoc2508.lhp.bk@gmail.com>
2023-05-14 19:27:20 +0700

    feat: authentication

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../Controllers/User.Controller");
 
-router.get("/register", UserController.registerUser);
-
+router.post("/register", UserController.registerUser);
+router.get("/login", UserController.loginUser);
 
 module.exports = router
